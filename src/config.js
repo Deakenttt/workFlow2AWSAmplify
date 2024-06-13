428c753d85d587b18fbb7c7a0112f9ee67b905c5
beibeit <beibeit@sfu.ca>
2024-06-12 20:45:11 -0700

    hide base url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // base url
 // export const BASE_URL = 'https://workflowbackendapi-production.up.railway.app';
-export const BASE_URL = secrets.BASE_URL
+export const BASE_URL = process.env.BASE_URL
