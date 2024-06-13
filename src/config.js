2667886391073a93f463868a82944eacfd4235b3
beibeit <beibeit@sfu.ca>
2024-06-12 20:03:40 -0700

    update base url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 // base url
-export const BASE_URL = 'https://workflowbackendapi-production.up.railway.app';
+// export const BASE_URL = 'https://workflowbackendapi-production.up.railway.app';
+export const BASE_URL = secrets.BASE_URL
