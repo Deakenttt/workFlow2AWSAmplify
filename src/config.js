c86e2960d3a620adc4a3eb5b33b6667ba41bc96b
beibeit <beibeit@sfu.ca>
2024-06-13 17:07:24 -0700

    update base url using vite

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 // base url
 // export const BASE_URL = 'https://workflowbackendapi-production.up.railway.app';
-export const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
+// in react 
+// export const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
+// in Vite 
+export const BASE_URL = `${import.meta.env.VITE_BASE_URL}`;
 console.log('Base URL:', BASE_URL);
